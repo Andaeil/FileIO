0de0832f636a53e8bdde6b8fa7c5487d821024cf
Andaeil <71754742+Andaeil@users.noreply.github.com>
2020-11-16 15:19:19 +0900

    Modified this to get a file name from the command line argument

--- src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
+++ src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
@@ -5,15 +5,17 @@ import java.io.*;
 /**
  * Created by sherxon on 4/23/17. https://github.com/sherxon/AlgoDS/tree/master/src/oi
  */
+
 public class UsingBufferedReader {
+	
     public static void main(String[] args) throws IOException {
-
-
-        //-------------- Test reading 1 MB file. --------------------
+    	String filename = (args[0]);
+    	
+        //-------------- Test reading file. --------------------
 
         StopWatch.start();
 
-        BufferedReader inputStream= new BufferedReader(new FileReader(DumpDataWriter.input1MB));
+        BufferedReader inputStream= new BufferedReader(new FileReader(filename));
         while (inputStream.read()!=-1){}
 
         long duration = StopWatch.stop();
@@ -21,18 +23,15 @@ public class UsingBufferedReader {
         
         inputStream.close();
 
+        /*StopWatch.start();
 
-        //-------------- Test reading 10 MB file. --------------------
-
-        StopWatch.start();
-
-        BufferedReader inputStream2= new BufferedReader(new FileReader(DumpDataWriter.input10MB));
+        BufferedReader inputStream2= new BufferedReader(new FileReader());
         while (inputStream2.read()!=-1){}
 
         long duration2 = StopWatch.stop();
         System.out.println(duration2 + " milsec");
 
-        inputStream2.close();
+        inputStream2.close();*/
 
         /*
         //-------------- Test reading 100 MB file. --------------------
